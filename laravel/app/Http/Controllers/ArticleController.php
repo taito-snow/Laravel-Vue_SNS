484d790cfafd52e922c2f0e943e19d21aea9a147
Taito Ito <taito.football@icloud.com>
2020-07-09 17:13:33 +0900

    Create CreateScreen

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -3,15 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    // 一覧
+    // 投稿一覧
     public function index ()
     {
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    // 投稿作成
+    public function create ()
+    {
+        return view('articles.create');
+    }
+
+    // 投稿送信
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $artivle->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    // 編集
+    public function edit (Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
     }
 }
