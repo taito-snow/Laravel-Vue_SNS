55abb317c3d476698c6fa4465347fbd1a77c333a
Taito Ito <taito.football@icloud.com>
2020-07-09 20:22:22 +0900

    Complete ShowScreen

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
     // 投稿一覧
     public function index ()
     {
@@ -25,7 +30,7 @@ class ArticleController extends Controller
     // 投稿送信
     public function store(ArticleRequest $request, Article $article)
     {
-        $artivle->fill($request->all());
+        $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
@@ -35,5 +40,25 @@ class ArticleController extends Controller
     public function edit (Article $article)
     {
         return view('articles.edit', ['article' => $article]);
+    }
+
+    // 更新
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('articles.index');
+    }
+
+    // 削除
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
+    // 詳細
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
     }
 }
