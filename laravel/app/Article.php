3bb9f3aaf51d72620d0f5c1419a440ef60daffd3
Taito Ito <taito.football@icloud.com>
2020-07-14 22:37:05 +0900

    Edit Tags

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -37,6 +37,6 @@ class Article extends Model
 
     public function tags(): BelongsToMany
     {
-        return $this->belongsToMany('App\Tag')-withTimestamps();
+        return $this->belongsToMany('App\Tag')->withTimestamps();
     }
 }
