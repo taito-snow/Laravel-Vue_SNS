c11adc83870c0b0bae962777c12c0986eeeae035
Taito Ito <taito.football@icloud.com>
2020-07-17 10:17:32 +0900

    Create Likes&Articles

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -10,8 +10,20 @@ class UserController extends Controller
     public function show(string $name)
     {
         $user= User::where('name', $name)->first();
+        $articles = $user->articles->sortByDesc('created_at');
         return view('users.show', [
             'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $articles = $user->likes->sortByDesc('created_at');
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
         ]);
     }
 
